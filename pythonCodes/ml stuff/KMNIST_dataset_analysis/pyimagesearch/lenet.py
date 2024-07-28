6671e7d33c36ce056801db8240497ae1d13c2028
Shubhojyoti Das <shubhojyotidas800@gmail.com>
2024-07-28 23:12:13 +0530

    update train.py with training code

--- pythonCodes/ml stuff/KMNIST_dataset_analysis/pyimagesearch/lenet.py
+++ pythonCodes/ml stuff/KMNIST_dataset_analysis/pyimagesearch/lenet.py
@@ -1,21 +1,20 @@
-from torch import conv2d
+from torch import conv2d, flatten
 from torch.nn import Module
 from torch.nn import Conv2d
 from torch.nn import Linear
 from torch.nn import MaxPool2d
 from torch.nn import ReLU
 from torch.nn import LogSoftmax
-from torch.nn import Flatten
 
 class LeNet(Module):
     def __init__(self, numChannels, classes):
         super(LeNet, self).__init__()
         
-        self.con2D1 = conv2d(in_channels = numChannels, out_channels = 20 kernel_size = (5,5))
+        self.con2D1 = conv2d(in_channels = numChannels, out_channels = 20, kernel_size = (5,5))
         self.relu1 = ReLU()
         self.maxpool1 = MaxPool2d(kernel_size = (2,2), stride = (2,2))
         
-        self.convsD2 = Conv2d(in_channels = 20, out_channels = 50, kernel_size = (5,5))
+        self.conv2D2 = Conv2d(in_channels = 20, out_channels = 50, kernel_size = (5,5))
         self.relu2 = ReLU()
         self.maxpool2 = MaxPool2d(kernel_size = (2,2), stride = (2,2))
         
@@ -24,3 +23,21 @@ class LeNet(Module):
         
         self.fc2 = Linear(in_features = 500, out_features = classes)
         self.logSoftmax = LogSoftmax(dim = 1)
+        
+    def forward(self, x):
+        x = self.conv2D1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+        
+        x = self.conv2D2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+        
+        x = flatten(x,1)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        
+        x = self.fc2(x)
+        x = self.logSoftmax(x)
+        
+        return x
